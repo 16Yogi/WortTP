a330826eaea06661b9e3d00ff22055aac5800742
Yogi <85734470+16Yogi@users.noreply.github.com>
2022-11-04 17:43:55 +0530

    Add files via upload

---
+++ WorldTP/index.php
@@ -0,0 +1,12 @@
+<?php
+   // include('nav.html');
+   include('banner.html');
+   // include('nav.html')
+   include('about.html');
+   // include('blog.html');
+   // // include('')
+   // include('customerRevice.html');
+   // include('team.html');
+   // include('runningProgram.html');
+   // include('footer.html');
+?>
